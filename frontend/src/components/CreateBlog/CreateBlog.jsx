904c09a3571dde6fe3622f22c050165668999b5a
kingjames500 <jimmymaina500@gmail.com>
2024-11-03 08:24:37 +0300

    uploading blog image to cloudinary

--- frontend/src/components/CreateBlog/CreateBlog.jsx
+++ frontend/src/components/CreateBlog/CreateBlog.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useMutation } from "react-query";
 import { Card } from "primereact/card";
 import { InputText } from "primereact/inputtext";
 import { Editor } from "primereact/editor";
@@ -7,29 +8,90 @@ import "primereact/resources/themes/saga-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import "quill/dist/quill.snow.css";
-import "./CreateBlog.css"; // Add custom styles here
+import "./CreateBlog.css";
+import apiUrl from "../../utils/apiUrl";
+import { Toaster, toast } from "sonner";
+import { uploadPreset, cloudName } from "../../utils/cloudinaryDetails";
+import { useNavigate } from "react-router-dom";
 
 const CreateBlog = () => {
   const [title, setTitle] = useState("");
   const [excerpt, setExcerpt] = useState("");
   const [content, setContent] = useState("");
-  const [image, setImage] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
 
-  const handleTextChange = (e) => {
-    setContent(e.htmlValue);
-  };
+  const redirect = useNavigate();
 
-  const handleImageChange = (e) => {
-    setImage(e.target.files[0]);
-  };
+  const { mutate, isLoading, isError, error } = useMutation({
+    mutationFn: async function (blogDetails) {
+      const response = await fetch(`${apiUrl}/create-blog`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(blogDetails),
+      });
+
+      if (response.ok === false) {
+        const error = await response.json();
+        throw new Error(error.message);
+      }
+    },
+    onSuccess: () => {
+      toast.success("Blog created successfully", {
+        duration: 5000,
+      });
+      setTimeout(() => {
+        redirect("/blogs");
+      }, 1500);
+    },
+
+    onError: (error) => {
+      toast.error(error.message, {
+        duration: 5000,
+      });
+    },
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const newBlog = {
+      title,
+      excerpt,
+      content,
+      imageUrl,
+    };
+    mutate(newBlog);
+  };
+
+  //uploading image to cloudinary and getting a secure url
+  const uploadImage = async (files) => {
+    const formData = new FormData();
+
+    formData.append("file", files[0]);
+    formData.append("upload_preset", uploadPreset);
+    const response = await fetch(
+      `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
+      {
+        method: "POST",
+        body: formData,
+      },
+    );
+    if (response.ok === false) {
+      const error = await response.json();
+      toast.error(error.message, {
+        duration: 2000,
+      });
+    }
+    const data = await response.json();
+    setImageUrl(data.secure_url);
+    console.log(data.secure_url);
   };
 
   return (
     <div className="form-container">
       <Card title="Create a Blog Post" className="p-shadow-3 custom-card">
+        <Toaster position="top-center" richColors expand={true} />
         <form onSubmit={handleSubmit}>
           <div className="p-field">
             <label htmlFor="title" className="p-d-block">
@@ -64,8 +126,7 @@ const CreateBlog = () => {
             <input
               type="file"
               id="image"
-              value={image}
-              onChange={handleImageChange}
+              onChange={(e) => uploadImage(e.target.files)}
               className="p-d-block"
             />
           </div>
@@ -77,7 +138,7 @@ const CreateBlog = () => {
             <Editor
               id="content"
               value={content}
-              onTextChange={handleTextChange}
+              onTextChange={(e) => setContent(e.htmlValue)}
               style={{ height: "13rem" }}
             />
           </div>
---
+++ frontend/src/utils/cloudinaryDetails.js
@@ -0,0 +1,6 @@
+const uploadPreset = import.meta.env.VITE_UPLOAD_PRESET;
+const cloudName = import.meta.env.VITE_CLOUD_NAME;
+// const apiKey = import.meta.env.VITE_API_KEY;
+// const apiSecret = import.meta.env.VITE_API_SECRET;
+
+export { uploadPreset, cloudName };
